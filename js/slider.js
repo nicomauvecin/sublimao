81c5b3fc89e849ba96dca7510419d8f3e8d488d7
nicomauvecin <medinamauvecin@gmail.com>
2021-03-22 22:30:21 -0300

    Agrega el proyecto completo

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+(function(){
+
+// Propiedades Slider
+
+var propSlider = {
+
+	slider: document.getElementById('slider'),
+	primerSlide: null
+
+}
+
+// Metodos slider
+
+var metSlider = {
+
+	inicio: function () {
+		setInterval(metSlider.moverSlide, 3000);
+	},
+
+	moverSlide: function () {
+		propSlider.slider.style.transition = 'all 1s ease';
+		propSlider.slider.style.marginLeft = '-100%';
+
+		setTimeout(function () {
+			propSlider.primerSlide = propSlider.slider.firstElementChild;
+			propSlider.slider.appendChild(propSlider.primerSlide);
+			propSlider.slider.style.transition = 'unset';
+			propSlider.slider.style.marginLeft = 0;
+		}, 1000);
+	}
+
+}
+
+metSlider.inicio();
+
+}())
